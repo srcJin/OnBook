a4acabb7b1a9e4e7db1b6737ad961f4b1c29b829
yutong195 <renyutong360@gmail.com>
2025-01-25 15:51:24 -0500

    Movement Data Recorded as Json

--- Assets/Scenes/Oculus Movement Scene Tryout/Scripts/FullBodyTrackingRecorder.cs
+++ Assets/Scenes/Oculus Movement Scene Tryout/Scripts/FullBodyTrackingRecorder.cs
@@ -1,117 +1,126 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-// public class FullBodyTrackingRecorder : MonoBehaviour
-// {
-//     public OVRBody ovrBody; // Reference to the OVRBody component in your scene
-//     private bool isRecording = false;
-//     private List<BodyFrameData> recordedData = new List<BodyFrameData>();
-//     private float recordingStartTime;
+public class FullBodyTrackingRecorder : MonoBehaviour
+{
+    public OVRBody ovrBody; // Reference to the OVRBody component in your scene
 
-//     [System.Serializable]
-//     public class BodyFrameData
-//     {
-//         public float timestamp; // Time since recording started
-//         public List<BoneData> bones = new List<BoneData>();
-//     }
+    private bool isRecording = false;
+    private List<BodyFrameData> recordedData = new List<BodyFrameData>();
+    private float recordingStartTime;
 
-//     [System.Serializable]
-//     public class BoneData
-//     {
-//         public string boneName;
-//         public Vector3 position;
-//         public Quaternion rotation;
-//     }
+    [System.Serializable]
+    public class BodyFrameData
+    {
+        public float timestamp; // Time since recording started
+        public List<BoneData> bones = new List<BoneData>();
+    }
 
-//     void Start()
-//     {
-//         Debug.Log("Press 'R' to start/stop recording.");
-//     }
+    [System.Serializable]
+    public class BoneData
+    {
+        public string boneName;
+        public Vector3 position;
+        public Quaternion rotation;
+    }
 
-//     void Update()
-//     {
-//         // Toggle recording with the 'R' key
-//         if (Input.GetKeyDown(KeyCode.R))
-//         {
-//             isRecording = !isRecording;
+    void Start()
+    {
+        Debug.Log("Press 'Space' to start/stop recording.");
+    }
 
-//             if (isRecording)
-//             {
-//                 Debug.Log("Recording started...");
-//                 recordedData.Clear();
-//                 recordingStartTime = Time.time; // Set the start time
-//             }
-//             else
-//             {
-//                 Debug.Log("Recording stopped. Saving data...");
-//                 SaveDataToJson();
-//             }
-//         }
-//     }
+    void Update()
+    {
+        // Toggle recording with the Space key
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isRecording = !isRecording;
 
-//     void FixedUpdate()
-//     {
-//         if (isRecording)
-//         {
-//             RecordFrameData();
-//         }
-//     }
+            if (isRecording)
+            {
+                Debug.Log("Recording started...");
+                recordedData.Clear();
+                recordingStartTime = Time.time; // Set the start time
+            }
+            else
+            {
+                Debug.Log("Recording stopped. Saving data...");
+                SaveDataToJson();
+            }
+        }
+    }
 
-//     void RecordFrameData()
-//     {
-//         if (ovrBody == null)
-//         {
-//             Debug.LogError("OVRBody component is not assigned.");
-//             return;
-//         }
+    void FixedUpdate()
+    {
+        if (isRecording)
+        {
+            RecordFrameData();
+        }
+    }
 
-//         BodyFrameData frameData = new BodyFrameData
-//         {
-//             timestamp = Time.time - recordingStartTime // Elapsed time since start
-//         };
+    void RecordFrameData()
+    {
+        if (ovrBody == null)
+        {
+            Debug.LogError("OVRBody component is not assigned.");
+            return;
+        }
 
-//         // Get the tracked joints from OVRBody
-//         var bodyJoints = ovrBody.; // Fetch all joints (bones)
-//         foreach (var joint in bodyJoints)
-//         {
-//             if (joint.Transform != null)
-//             {
-//                 BoneData boneData = new BoneData
-//                 {
-//                     boneName = joint.Id.ToString(),
-//                     position = joint.Transform.position,
-//                     rotation = joint.Transform.rotation
-//                 };
-//                 frameData.bones.Add(boneData);
-//             }
-//         }
+        if (!ovrBody.BodyState.HasValue)
+        {
+            Debug.LogWarning("Body tracking data is not available.");
+            return;
+        }
 
-//         recordedData.Add(frameData);
-//     }
+        BodyFrameData frameData = new BodyFrameData
+        {
+            timestamp = Time.time - recordingStartTime // Elapsed time since start
+        };
 
-//     void SaveDataToJson()
-//     {
-//         if (recordedData.Count == 0)
-//         {
-//             Debug.LogWarning("No data recorded. JSON file will be empty.");
-//             return;
-//         }
+        // Get the joint locations from OVRBody
+        var jointLocations = ovrBody.BodyState.Value.JointLocations;
+        for (int i = 0; i < jointLocations.Length; i++)
+        {
+            var joint = jointLocations[i];
+            if (joint.PositionValid || joint.OrientationValid)
+            {
+                BoneData boneData = new BoneData
+                {
+                    boneName = ((OVRPlugin.BoneId)i).ToString(),
+                    position = new Vector3(joint.Pose.Position.x, joint.Pose.Position.y, joint.Pose.Position.z),
+                    rotation = new Quaternion(joint.Pose.Orientation.x, joint.Pose.Orientation.y, joint.Pose.Orientation.z, joint.Pose.Orientation.w)
+                };
+                frameData.bones.Add(boneData);
+            }
+        }
 
-//         // Convert recorded data to JSON
-//         string json = JsonUtility.ToJson(new Wrapper<BodyFrameData> { frames = recordedData }, true);
+        recordedData.Add(frameData);
+    }
 
-//         // Save JSON to local storage
-//         string filePath = Path.Combine(Application.persistentDataPath, "body_tracking_data.json");
-//         File.WriteAllText(filePath, json);
+    void SaveDataToJson()
+    {
+        if (recordedData.Count == 0)
+        {
+            Debug.LogWarning("No data recorded. JSON file will be empty.");
+            return;
+        }
 
-//         Debug.Log($"Data saved to {filePath}");
-//     }
+        // Convert recorded data to JSON
+        string json = JsonUtility.ToJson(new Wrapper<BodyFrameData> { frames = recordedData }, true);
 
-//     // Wrapper class for JSON serialization
-//     [System.Serializable]
-//     public class Wrapper<T>
-//     {
-//         public List<T> frames;
-//     }
-// }
+        // Save JSON to local storage
+        string filePath = Path.Combine(Application.persistentDataPath, "body_tracking_data.json");
+        File.WriteAllText(filePath, json);
+
+        Debug.Log($"Data saved to {filePath}");
+    }
+
+    // Wrapper class for JSON serialization
+    [System.Serializable]
+    public class Wrapper<T>
+    {
+        public List<T> frames;
+    }
+}
